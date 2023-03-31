97b7a8a9795153215485d26e5dec36e6e11aa6fc
Yaroslav Galkin <galkinyaroslav@gmail.com>
2023-03-31 19:27:12 +0300

    add data to db

--- shop_project/catalog/admin.py
+++ shop_project/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from catalog.models import Category, Producer, Discount, Promocode, Product
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Producer)
+admin.site.register(Discount)
+admin.site.register(Promocode)
+admin.site.register(Product)
+
--- shop_project/users/models.py
+++ shop_project/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser#, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 
@@ -18,10 +18,12 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50, blank=True, null=True, default=None)
     last_name = models.CharField(max_length=50, blank=True, null=True, default=None)
@@ -29,8 +31,8 @@ class CustomUser(AbstractBaseUser):
     cashback_points = models.IntegerField(default=0)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
